b0ce2afafc008eb6f42ac5eea0c325e762c3289c
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-09 00:05:06 +0700

    add auth service

---
+++ auth-service/src/main/java/com/service/auth/authServiceApplication.java
@@ -0,0 +1,15 @@
+package com.service.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class authServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(authServiceApplication.class, args);
+	}
+
+}
