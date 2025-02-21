8953b99b0c7bd0628f649ea88b00aad6ead38704
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-22 00:08:45 +0700

    update AuthServiceApplication.java

---
+++ auth-service/src/main/java/com/service/auth/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.service.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.service.auth")
+public class AuthServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AuthServiceApplication.class, args);
+  }
+}
