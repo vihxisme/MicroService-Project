888790a7dc37895cab2b16b5b15e8f17d36ea06e
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-21 23:12:13 +0700

    update AuthService.java(auth-service)

--- auth-service/src/main/java/com/service/auth/services/impl/AuthService.java
+++ auth-service/src/main/java/com/service/auth/services/impl/AuthService.java
@@ -3,30 +3,50 @@ package com.service.auth.services.impl;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.auth.dtos.LoginDTO;
 import com.service.auth.entities.User;
+import com.service.auth.repositories.ApiCustomerClient;
 import com.service.auth.repositories.UserRepository;
+import com.service.auth.requests.AddInfoCustomerRequest;
 import com.service.auth.requests.LoginRequest;
 import com.service.auth.requests.RegisterUserRequest;
 import com.service.auth.resources.LoginResource;
 import com.service.auth.responses.ErrorResponse;
+import com.service.auth.responses.SuccessResponse;
 import com.service.auth.securities.JwtTokenProvider;
 import com.service.auth.services.interfaces.AuthInterface;
+import com.service.auth.wrappers.RegisterUserRequestWrapper;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class AuthService implements AuthInterface {
+
+  private Logger logger = LoggerFactory.getLogger(AuthService.class);
 
   @Autowired
   private UserRepository userRepository;
 
   @Autowired
+  private ApiCustomerClient apiCustomerClient;
+
+  @Autowired
   private JwtTokenProvider jwtTokenProvider;
+
+  @Autowired
+  private ObjectMapper objectMapper;
 
   private User authenticateUser(String identifier, String password) {
     Optional<User> user = userRepository.findByUsernameOrEmail(identifier, identifier);
@@ -45,8 +65,15 @@ public class AuthService implements AuthInterface {
         throw new BadCredentialsException("Invalid username/email or password");
       }
 
+      ResponseEntity<?> response = apiCustomerClient.getUserIdByAuthId(authenticatedUser.getId().toString());
+
+      SuccessResponse<?> successResponse = objectMapper.convertValue(response.getBody(),
+          new TypeReference<SuccessResponse<UUID>>() {
+          });
+
       LoginDTO loginDTO = new LoginDTO(
           authenticatedUser.getId(),
+          UUID.fromString(successResponse.getData().toString()),
           authenticatedUser.getUsername(),
           authenticatedUser.getEmail(),
           authenticatedUser.getRole().toString());
@@ -69,7 +96,6 @@ public class AuthService implements AuthInterface {
 
   @Override
   public Object registerUser(RegisterUserRequest registerUserRequest) {
-    // try {
     String password = registerUserRequest.getPassword();
     String confirmPassword = registerUserRequest.getConfirmPassword();
 
@@ -83,6 +109,35 @@ public class AuthService implements AuthInterface {
     user.setPassword(new BCryptPasswordEncoder().encode(registerUserRequest.getPassword()));
 
     return userRepository.save(user);
+  }
+
+  @Override
+  @Transactional
+  public Object registerUser(RegisterUserRequestWrapper request) {
+    String password = request.getRegisterUserRequest().getPassword();
+    String confirmPassword = request.getRegisterUserRequest().getConfirmPassword();
+
+    if (!password.equals(confirmPassword)) {
+      throw new IllegalArgumentException("Password and Confirm Password must be the same");
+    }
+
+    User user = new User();
+    user.setUsername(request.getRegisterUserRequest().getUsername());
+    user.setEmail(request.getRegisterUserRequest().getEmail());
+    user.setPassword(new BCryptPasswordEncoder().encode(request.getRegisterUserRequest().getPassword()));
+
+    User addUser = userRepository.save(user);
+
+    AddInfoCustomerRequest addInfoCustomerRequest = new AddInfoCustomerRequest(
+        addUser.getId(),
+        request.getInfoCustomer().getFirstName(),
+        request.getInfoCustomer().getLastName(),
+        addUser.getEmail(),
+        request.getInfoCustomer().getPhone());
+
+    apiCustomerClient.addInfoCustomer(addInfoCustomerRequest);
+
+    return addUser;
   }
 
 }
