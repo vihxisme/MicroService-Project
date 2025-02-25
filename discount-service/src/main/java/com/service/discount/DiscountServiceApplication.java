cc4500ed8fb0e6369ea3a0dfb925abca9983167e
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-25 20:30:35 +0700

    register with eureka

--- discount-service/src/main/java/com/service/discount/DiscountServiceApplication.java
+++ discount-service/src/main/java/com/service/discount/DiscountServiceApplication.java
@@ -2,7 +2,9 @@ package com.service.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DiscountServiceApplication {
   public static void main(String[] args) {
