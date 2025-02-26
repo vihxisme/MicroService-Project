efb58758cfb17c0487da810331171b05ef5856ae
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-27 02:52:16 +0700

    add openfeign/ call api to api-composition

---
+++ discount-service/src/main/java/com/service/discount/repositories/ApiClient.java
@@ -0,0 +1,13 @@
+package com.service.discount.repositories;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "api-composition")
+public interface ApiClient {
+
+  @GetMapping("/discount-client/target/internal/discount-client/with-target")
+  ResponseEntity<?> getDiscountTargetWithTargetIdName(@RequestParam int page, @RequestParam int size);
+}
