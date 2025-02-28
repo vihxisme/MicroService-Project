562fe2d269e42b25a06bb3a6babb4200c07cae14
vihxisme <nguyenvanvinh9203@gmail.com>
2025-02-28 17:35:20 +0700

    add inventory-service

---
+++ inventory-service/src/main/java/com/service/inventory/InvetoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.service.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvetoryServiceApplication.class, args);
+    }
+}
