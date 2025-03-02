c377c38c670f25ce517f8006dcffba422d42c4d1
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-02 22:15:54 +0700

    add file ProdVariantResource.java

---
+++ product-service/src/main/java/com/service/product/resources/ProdVariantResource.java
@@ -0,0 +1,17 @@
+package com.service.product.resources;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+@Builder
+public class ProdVariantResource {
+
+    private final int id;
+    private final int colorId;
+    private final String colorName;
+    private final int sizeId;
+    private final String sizeName;
+}
