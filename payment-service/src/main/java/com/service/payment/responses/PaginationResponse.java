34732f309c17065a87631af695cf4b4c57d7962f
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-06 16:41:25 +0700

    add responses/

---
+++ payment-service/src/main/java/com/service/payment/responses/PaginationResponse.java
@@ -0,0 +1,19 @@
+package com.service.payment.responses;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+@Builder
+public class PaginationResponse<T> {
+
+    private final List<T> content;
+    private final int pageNumber;
+    private final int pageSize;
+    private final int totalPages;
+    private final long totalElements;
+}
