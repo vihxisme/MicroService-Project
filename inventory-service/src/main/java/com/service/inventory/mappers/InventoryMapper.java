b503aee3b34733c0c8a41d5b389bc7709ed2ac73
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-28 13:40:44 +0700

    create: mappers

--- inventory-service/src/main/java/com/service/inventory/mappers/InventoryMapper.java
+++ inventory-service/src/main/java/com/service/inventory/mappers/InventoryMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.service.inventory.entities.Inventory;
 import com.service.inventory.requests.InventoryRequest;
+import com.service.events.dto.InventoryDTO;
 import com.service.inventory.resources.InventoryResource;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
@@ -16,4 +17,6 @@ public interface InventoryMapper {
     void updateInventoryFromRequest(InventoryRequest request, @MappingTarget Inventory inventory);
 
     InventoryResource toInventoryResource(Inventory inventory);
+
+    InventoryRequest toInventoryRequest(InventoryDTO request);
 }
