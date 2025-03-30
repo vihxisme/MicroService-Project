2742faaadf105352dbbed653448d0043e4c22d73
vihxisme <nguyenvanvinh9203@gmail.com>
2025-03-30 14:32:35 +0700

    public endpoint

--- api-gateway/src/main/java/com/service/apigateway/configurations/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/service/apigateway/configurations/AuthenticationFilter.java
@@ -31,82 +31,83 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthenticationFilter implements GlobalFilter, Ordered {
 
-  private final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+    private final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
-  @Autowired
-  private IdentityService identityService;
+    @Autowired
+    private IdentityService identityService;
 
-  private final String[] PUBLIC_ENDPOINT = {
-      "/identity/auth/.*",
-      "/identity/authenticated/.*"
-  };
+    private final String[] PUBLIC_ENDPOINT = {
+        "/identity/auth/.*",
+        "/identity/authenticated/.*",
+        "/product/v1/products/only-discount",
+        "/v1/products/info/all",};
 
-  @Value("${app.api-prefix}")
-  private String apiPrefix;
+    @Value("${app.api-prefix}")
+    private String apiPrefix;
 
-  @Override
-  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    ServerHttpRequest request = exchange.getRequest();
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
 
-    // Kiểm tra nếu request là public API (bỏ qua xác thực)
-    if (isPublicEndpoint(request)) {
-      return chain.filter(exchange);
+        // Kiểm tra nếu request là public API (bỏ qua xác thực)
+        if (isPublicEndpoint(request)) {
+            return chain.filter(exchange);
+        }
+
+        // Lấy token từ Authorization header
+        List<String> authHeaders = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
+        if (authHeaders == null || authHeaders.isEmpty()) {
+            return unauthorizedResponse(exchange.getResponse());
+        }
+
+        String token = authHeaders.get(0);
+
+        // Gọi IdentityService để xác thực token
+        return identityService.validateToken(token)
+                .flatMap(response -> {
+                    if (response.getData().getValid()) {
+                        return chain.filter(exchange); // Token hợp lệ, tiếp tục request
+                    } else {
+                        return unauthorizedResponse(exchange.getResponse()); // Token không hợp lệ
+                    }
+                })
+                .onErrorResume(e -> {
+                    logger.error("Error during token validation", e); // Thêm log lỗi chi tiết
+                    return unauthorizedResponse(exchange.getResponse()); // Xử lý lỗi nếu có
+                });
     }
 
-    // Lấy token từ Authorization header
-    List<String> authHeaders = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
-    if (authHeaders == null || authHeaders.isEmpty()) {
-      return unauthorizedResponse(exchange.getResponse());
+    private boolean isPublicEndpoint(ServerHttpRequest request) {
+        String path = request.getURI().getPath();
+        return Arrays.stream(PUBLIC_ENDPOINT)
+                .anyMatch(endpoint -> path.matches(apiPrefix + endpoint));
     }
 
-    String token = authHeaders.get(0);
+    private Mono<Void> unauthorizedResponse(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
 
-    // Gọi IdentityService để xác thực token
-    return identityService.validateToken(token)
-        .flatMap(response -> {
-          if (response.getData().getValid()) {
-            return chain.filter(exchange); // Token hợp lệ, tiếp tục request
-          } else {
-            return unauthorizedResponse(exchange.getResponse()); // Token không hợp lệ
-          }
-        })
-        .onErrorResume(e -> {
-          logger.error("Error during token validation", e); // Thêm log lỗi chi tiết
-          return unauthorizedResponse(exchange.getResponse()); // Xử lý lỗi nếu có
-        });
-  }
+        // Tạo lỗi response
+        Map<String, String> errors = new HashMap<>();
+        HttpStatus statusCode = (HttpStatus) response.getStatusCode();
+        errors.put("message", statusCode != null ? statusCode.toString() : "Unknown error");
 
-  private boolean isPublicEndpoint(ServerHttpRequest request) {
-    String path = request.getURI().getPath();
-    return Arrays.stream(PUBLIC_ENDPOINT)
-        .anyMatch(endpoint -> path.matches(apiPrefix + endpoint));
-  }
+        ErrorResponse errorResponse = new ErrorResponse(401, "Unauthorized", errors);
 
-  private Mono<Void> unauthorizedResponse(ServerHttpResponse response) {
-    response.setStatusCode(HttpStatus.UNAUTHORIZED);
-    response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-
-    // Tạo lỗi response
-    Map<String, String> errors = new HashMap<>();
-    HttpStatus statusCode = (HttpStatus) response.getStatusCode();
-    errors.put("message", statusCode != null ? statusCode.toString() : "Unknown error");
-
-    ErrorResponse errorResponse = new ErrorResponse(401, "Unauthorized", errors);
-
-    // Chuyển thành JSON
-    ObjectMapper objectMapper = new ObjectMapper();
-    try {
-      byte[] responseBody = objectMapper.writeValueAsBytes(errorResponse);
-      DataBuffer buffer = response.bufferFactory().wrap(responseBody);
-      return response.writeWith(Mono.just(buffer));
-    } catch (JsonProcessingException e) {
-      return response.setComplete();
+        // Chuyển thành JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            byte[] responseBody = objectMapper.writeValueAsBytes(errorResponse);
+            DataBuffer buffer = response.bufferFactory().wrap(responseBody);
+            return response.writeWith(Mono.just(buffer));
+        } catch (JsonProcessingException e) {
+            return response.setComplete();
+        }
     }
-  }
 
-  @Override
-  public int getOrder() {
-    return -1;
-  }
+    @Override
+    public int getOrder() {
+        return -1;
+    }
 
 }
