384b96a1f7b7f60d3d13a711e7a6b3b5f3981c36
vihxisme <nguyenvanvinh9203@gmail.com>
2025-05-09 00:29:06 +0700

    add notification-service

---
+++ notification-service/src/test/java/com/service/notification/NotificationServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.service.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
